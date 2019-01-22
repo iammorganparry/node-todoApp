2d84b919594a35a2fad1dd44a456e0a894c64a1a
Morgan Parry <morgan.parry@cloud-iq.com>
2019-01-22 11:44:43 +0000

    add GET csv route

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -18,6 +18,14 @@ router.get('/', async (req,res) => {
 
 router.get('/:id', async(req,res) => {
     let results = await queryData(req,res)
+    results.forEach((row) => {
+        res.send(row);
+    });
+});
+
+//CSV Route
+router.get('/CSV/:id',async (req,res) => {
+    let results = await getCsvData(req,res)
     res.send(results);
 });
 
@@ -47,6 +55,34 @@ async function getAllCampaignData(req,res) {
         // Runs the query
         const [rows] = await bigquery.query(options)
         return rows;
+}
+
+function getCsvData(req,res) {
+    return new Promise(async (resolve,reject) => {
+        let tag_id = req.params.id;
+        
+            const sqlQuery = `SELECT end_ts order_ts,order_id,cart_last_total basket_value,cart_currency currency_code,cart_payment_method payment_method, event_ts as email_ts,profile_email email_address
+            FROM analytics_platform.fact_interaction_info 
+            LEFT OUTER JOIN analytics_platform.dim_exchange_rate
+            ON (COALESCE(cart_currency, 'GBP') = base_currency AND target_currency = 'GBP')
+            WHERE tag_id = '${tag_id}'
+            AND end_ts <= CURRENT_TIMESTAMP()
+            AND is_cancel = true
+            AND cart_last_total > 0
+            AND recovery_method IS NOT NULL
+            ORDER BY end_ts;`;
+            const options = {
+                query: sqlQuery
+            }
+            // Runs the query
+            const [rows] = await bigquery.query(options)
+            if(rows.length > 0) {
+                resolve(rows);
+            } else {
+                reject(err);
+            }
+            // return rows;
+    });
 }
 
 module.exports = router;
